daad71a8d1cdb83c3d7654da5fe839fea5e21179
zhangsan <zhangsanitcat.cn>
2020-06-28 22:15:09 +0800

    修改了启动类

--- src/main/java/com/jwt/BootDemoApplication.java
+++ src/main/java/com/jwt/BootDemoApplication.java
@@ -10,6 +10,7 @@ public class BootDemoApplication {
     public static void main(String[] args) {
         Integer i = 20;
         System.out.println(20);
+        System.out.println("String");
         SpringApplication.run(BootDemoApplication.class);
     }
 }
